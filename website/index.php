d2880e8b255fd49a28673c0beed6be8a5e89b805
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2023-09-28 17:11:52 +0600

    Initial commit

---
+++ website/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="image/favicon.png">
+    <title>AcademiXO</title>
+    
+    
+</head>
+<body>
+    <?php include("header.html");  ?>
+<main>
+    <h2 style="text-align: center;">Welcome Champ! Explore your thirst here</h2>
+</main>
+<hr>
+<?php include("footer.html");  ?>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
